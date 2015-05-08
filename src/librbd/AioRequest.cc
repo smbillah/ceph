35f973a7c58606c10c0e85d313d745833127a563
Jason Dillaman <dillaman@redhat.com>
2015-05-08 14:32:04 -0400

    librbd: include actual write op type in AbstractWrite debug output
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    
    Conflicts:
    	src/librbd/AioRequest.cc

--- src/librbd/AioRequest.cc
+++ src/librbd/AioRequest.cc
@@ -305,8 +305,8 @@ namespace librbd {
 
   bool AbstractWrite::should_complete(int r)
   {
-    ldout(m_ictx->cct, 20) << "write " << this << " " << m_oid << " "
-                           << m_object_off << "~" << m_object_len
+    ldout(m_ictx->cct, 20) << get_write_type() << " " << this << " " << m_oid
+                           << " " << m_object_off << "~" << m_object_len
 			   << " should_complete: r = " << r << dendl;
 
     bool finished = true;
@@ -391,8 +391,9 @@ namespace librbd {
 
   int AbstractWrite::send() {
     assert(m_ictx->owner_lock.is_locked());
-    ldout(m_ictx->cct, 20) << "send " << this << " " << m_oid << " "
-			   << m_object_off << "~" << m_object_len << dendl;
+    ldout(m_ictx->cct, 20) << "send " << get_write_type() << " " << this <<" "
+                           << m_oid << " " << m_object_off << "~"
+                           << m_object_len << dendl;
 
     if (!send_pre()) {
       return 0;
